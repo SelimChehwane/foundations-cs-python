c29d3c6954a8746c2edbfd36470df21e55a74b74
SelimChehwane <selim_chehwane@hotmail.com>
2023-10-25 18:12:01 +0300

    checKRotation Update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+                                        ###############
+                                        # Assignment 3#
+                                        ###############
 def displayYMenu():
     print("Menu:\n" + "\t1.Add Matrices\n"+"\t2.Check Rotation\n"+"\t3.Invert Dictionary\n"
           +"\t 4.Covert Matrix to Dictionary \n"+"\t5.Check Palindrome\n"
@@ -45,6 +48,54 @@ def addMatrices():
     print(matrix1, "+", matrix2, "=", matrix3)
 # print the result
 # the time complexity is N^2 because we have 3 nested loops that are dependent on user input
+
+def checKRotation():
+    matrix1 = []
+    matrix2 = []
+    rows = int(input("Enter number of rows of matrix1: "))
+    columns = int(input("Enter number of columns of matrix1: "))
+    for i in range(rows):
+        row = []
+# create sub-list row
+        for j in range(columns):
+            value = int(input("Enter the elements of column {}".format((j + 1)) + " at row {}".format(i + 1)
+                              + " of the first Matrix: "))
+# user input of elements in matrix1
+            row.append(value)
+# append the input element to sub-list row
+        matrix1.append(row)
+# append the sub-list row to matrix1
+
+    rows = int(input("Enter number of rows of matrix2: "))
+    columns = int(input("Enter number of columns of matrix2: "))
+    for i in range(rows):
+        row = []
+# create sub-list row
+        for j in range(columns):
+            value = int(input("Enter the elements of column {}".format((j + 1)) + " at row {}".format(i + 1)
+                              + " of the second Matrix: "))
+# user input of elements in matrix2
+            row.append(value)
+# append the input element to sub-list row
+        matrix2.append(row)
+# append the sub-list row to matrix2
+    if len(matrix1) != len(matrix2[0]) or len(matrix1[0]) != len(matrix2):
+        result = False
+# if the columns of X are not rows in Y and the rows in Y are nt columns in X it is false
+    for i in range(len(matrix1)):
+# we take the lenghth of matrix1 (columns) and iterate over it
+        for j in range(len(matrix2[0])):
+# we iterate over the indexed matrix2 (rows)
+            if matrix1[i][j] != matrix2[j][i]:
+                result = False
+            else:
+                result = True
+# we compare matrix1 and matrix2 at [i][j] to determine if it is a rotation
+    print("It is", result, "that", matrix1, "is a rotation of", matrix2)
+
+
+
+
 
 
 
