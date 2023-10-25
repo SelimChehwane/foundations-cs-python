d85a0b87983c7bc3fde9d07db88d1346023272ad
SelimChehwane <selim_chehwane@hotmail.com>
2023-10-25 13:40:17 +0300

    Assignment 3

---
+++ main.py
@@ -0,0 +1,5 @@
+def displayYMenu():
+    print("Menu:\n" + "\t1.Add Matrices\n"+"\t2.Check Rotation\n"+"\t3.Invert Dictionary\n"
+          +"\t4.Covert Matrix to Dictionary\n"+"\t5.Check Palindrome\n"
+          +"\t6.Search for an Element & Merge Sort\n"+"\t7.Exit\n")
+
