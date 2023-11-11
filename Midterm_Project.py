92f4a262bd3d78df8b63a8158998f17a0910a363
SelimChehwane <selim_chehwane@hotmail.com>
2023-11-11 11:37:10 +0200

    Creating --> displayMenu

---
+++ Midterm_Project.py
@@ -0,0 +1,6 @@
+def displayMenu():
+    print("-------------------------Hello there! Welcome to my Midterm Project-------------------------")
+    print()
+    print("Please select an option to start:\n 1.Open Tab\n 2.Close Tab\n 3.Switch Tab\n 4.Display All Tabs\n 5.Open Nested Tab\n 6.Clear All Tabs\n 7.Save Tabs\n 8.Import tabs\n 9.Exit")
+
+
