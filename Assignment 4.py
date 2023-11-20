8aa8ee10736973bd4a383b1a2f73850e77ef7c1c
SelimChehwane <selim_chehwane@hotmail.com>
2023-11-20 12:34:48 +0200

    Creating  --> displayMenu

---
+++ Assignment 4.py
@@ -0,0 +1,13 @@
+def displayMenu():
+    user_name = input("Please enter your name: ")
+    print()
+    print("Hello", user_name,", welcome to my fourth assignment!")
+    print()
+    print("Please select an option to start: " 
+          "\n 1.Singly Linked list"
+          "\n 2.Check if Palindrome"
+          "\n 3.Priority Queue"
+          "\n 4.Evaluate and Infix Expression"
+          "\n 5.Graph"
+          "\n 6.Exit")
+
