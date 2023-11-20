7facc96fb984cc3360223db73c38cef4366447f6
SelimChehwane <selim_chehwane@hotmail.com>
2023-11-20 12:42:21 +0200

    Creating  --> nodeMenu

--- Assignment 4.py
+++ Assignment 4.py
@@ -11,3 +11,11 @@ def displayMenu():
           "\n 5.Graph"
           "\n 6.Exit")
 
+def nodeMenu():
+    print("Welcome to the Nodes Menu: "
+          "\n A. Add Nodes"
+          "\n B.Display Nodes"
+          "\n C.Search for & Delete Node"
+          "\n D.Return to Main Menu")
+
+
